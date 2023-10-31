91754cf630f5f08bd7e2f3f2b9dc1bbc901ac674
Neres <neresdev@gmail.com>
2023-10-31 15:13:17 -0300

    adding repository for STUDENT table

--- src/main/java/com/nrs/school/back/repository/StudentRepostitory.java
+++ src/main/java/com/nrs/school/back/repository/StudentRepostitory.java
@@ -1,2 +1,14 @@
-package com.nrs.school.back.repository;public class StudentRepostitory {
+package com.nrs.school.back.repository;
+
+import com.nrs.school.back.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface StudentRepostitory extends JpaRepository<Student, Integer> {
+    @Query("SELECT name FROM Student")
+    List<String> findAllStudents();
 }
