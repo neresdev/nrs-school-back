55e7c9d009330dfb541acd9108c316b6eb7c5bf4
Neres <neresdev@gmail.com>
2023-10-31 15:07:51 -0300

    adding configuration for local setup

--- src/main/java/com/nrs/school/back/config/LocalConfig.java
+++ src/main/java/com/nrs/school/back/config/LocalConfig.java
@@ -1,2 +1,26 @@
-package com.nrs.school.back.config;public class LocalConfig {
+package com.nrs.school.back.config;
+
+import com.nrs.school.back.entities.Student;
+import com.nrs.school.back.repository.StudentRepostitory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private StudentRepostitory repository;
+
+    @Bean
+    public void startDB(){
+        Student student1 = new Student(1, "Student 1");
+        Student student2 = new Student(2, "Student 2");
+
+        repository.saveAll(List.of(student1, student2));
+    }
 }
