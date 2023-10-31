3ae3ad00035ce6954c342f569aa4f6b0d0274b9f
Neres <neresdev@gmail.com>
2023-10-31 15:12:54 -0300

    adding DTO

--- src/main/java/com/nrs/school/back/entities/dto/StudentDTO.java
+++ src/main/java/com/nrs/school/back/entities/dto/StudentDTO.java
@@ -1,2 +1,13 @@
-package com.nrs.school.back.entities.dto;public class StudentDTO {
+package com.nrs.school.back.entities.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StudentDTO {
+    private Integer id;
+    private String name;
 }
