a65d69d545f564d0e709c1ca6c42373248d6feae
Neres <neresdev@gmail.com>
2023-11-18 07:25:19 -0300

    adding TODO: implement using mapstruct

--- src/main/java/com/nrs/school/back/mapper/StudentsMapper.java
+++ src/main/java/com/nrs/school/back/mapper/StudentsMapper.java
@@ -1,13 +1,16 @@
-package com.nrs.school.back.mapper;
-
-import com.nrs.school.back.entities.Students;
-import com.nrs.school.back.entities.dto.StudentsDTO;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-
-@Mapper
-public interface StudentsMapperasdasd {
-    @Mapping(source = "id", target = "id")
-    @Mapping(source = "name", target = "name")
-    StudentsDTO map(Students students);
-}
+// TODO this mapper with mapstruct
+//package com.nrs.school.back.mapper;
+//
+//import com.nrs.school.back.entities.Students;
+//import com.nrs.school.back.entities.dto.StudentsDTO;
+//import org.mapstruct.Mapper;
+//import org.mapstruct.Mapping;
+//import org.mapstruct.factory.Mappers;
+//
+//@Mapper(componentModel = "spring")
+//public interface StudentsMapper {
+//    @Mapping(source = "id", target = "id")
+//    @Mapping(source = "name", target = "name")
+//    StudentsDTO map(Students students);
+//
+//}
