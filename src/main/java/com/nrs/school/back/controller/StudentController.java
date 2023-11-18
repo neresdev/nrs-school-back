dc5710b65b353eaa7fedfad2689953d5d4ca91ce
Neres <neresdev@gmail.com>
2023-11-18 07:21:18 -0300

    adding rest controller

--- src/main/java/com/nrs/school/back/controller/StudentController.java
+++ src/main/java/com/nrs/school/back/controller/StudentController.java
@@ -1,2 +1,33 @@
-package com.nrs.school.back.controller;public class StudentController {
+package com.nrs.school.back.controller;
+
+import com.nrs.school.back.entities.Students;
+import com.nrs.school.back.entities.dto.StudentsDTO;
+import com.nrs.school.back.service.StudentsService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping(value = "/api")
+public class StudentController {
+
+    private static final String ID = "/{id}";
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @Autowired
+    private StudentsService service;
+
+    @GetMapping("/all/students")
+    public ResponseEntity<List<StudentsDTO>> findAll(){
+        return ResponseEntity.ok()
+                .body(service.findAll().stream().map(u -> mapper.map(u, StudentsDTO.class)).collect(Collectors.toList()));
+    }
 }
