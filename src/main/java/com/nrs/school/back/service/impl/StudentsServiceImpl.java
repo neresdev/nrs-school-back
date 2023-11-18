7d55c546fa5e49fceb8d0adff55610a7ba415b1e
Neres <neresdev@gmail.com>
2023-11-18 07:27:09 -0300

    adding service

--- src/main/java/com/nrs/school/back/service/impl/StudentsServiceImpl.java
+++ src/main/java/com/nrs/school/back/service/impl/StudentsServiceImpl.java
@@ -1,2 +1,36 @@
-package com.nrs.school.back.service.impl;public class StudentsServiceImpl {
+package com.nrs.school.back.service.impl;
+
+import com.nrs.school.back.entities.Students;
+import com.nrs.school.back.entities.dto.StudentsDTO;
+import com.nrs.school.back.service.StudentsService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class StudentsServiceImpl implements StudentsService {
+    @Override
+    public Students findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Students> findAll() {
+        return List.of(new Students(1, "uai"), new Students(2, "uei"));
+    }
+
+    @Override
+    public Students create(StudentsDTO studentsDTO) {
+        return null;
+    }
+
+    @Override
+    public Students update(StudentsDTO studentsDTO) {
+        return null;
+    }
+
+    @Override
+    public void delete(Integer studentId) {
+
+    }
 }
