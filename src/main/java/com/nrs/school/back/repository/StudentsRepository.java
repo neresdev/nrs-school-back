df21aa7a3e185763b634002798cf5d275bc5089f
Neres <neresdev@gmail.com>
2023-11-23 17:22:00 -0300

    removing methods

--- src/main/java/com/nrs/school/back/repository/StudentsRepository.java
+++ src/main/java/com/nrs/school/back/repository/StudentsRepository.java
@@ -2,17 +2,10 @@ package com.nrs.school.back.repository;
 
 import com.nrs.school.back.entities.Students;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface StudentsRepository extends JpaRepository<Students, Integer> {
-
-    @Query("SELECT name from Students")
-    List<String> findAllStudents();
-
-    List<Students> findAll();
 
 }
