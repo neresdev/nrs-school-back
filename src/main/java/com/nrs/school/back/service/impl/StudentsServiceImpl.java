b2ffec6eacb09988def114ed13d9cc24b96ba456
Neres <neresdev@gmail.com>
2023-11-23 17:23:57 -0300

    moving Mapper for this class and feature for find student by Id

--- src/main/java/com/nrs/school/back/service/impl/StudentsServiceImpl.java
+++ src/main/java/com/nrs/school/back/service/impl/StudentsServiceImpl.java
@@ -2,27 +2,37 @@ package com.nrs.school.back.service.impl;
 
 import com.nrs.school.back.entities.Students;
 import com.nrs.school.back.entities.dto.StudentsDTO;
+import com.nrs.school.back.exceptions.ObjectNotFoundException;
 import com.nrs.school.back.repository.StudentsRepository;
 import com.nrs.school.back.service.StudentsService;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class StudentsServiceImpl implements StudentsService {
 
+    public static final String NOT_FOUND_MESSAGE = "Objeto nao encontrado";
+
     @Autowired
     private StudentsRepository repository;
 
+    @Autowired
+    private ModelMapper mapper;
+    
     @Override
-    public Students findById(Integer id) {
-        return null;
+    public List<StudentsDTO> findAll() {
+        return repository.findAll().stream().map(u -> mapper.map(u, StudentsDTO.class)).collect(Collectors.toList());
     }
 
     @Override
-    public List<Students> findAll() {
-        return repository.findAll();
+    public StudentsDTO findById(Integer id) {
+        return mapper.map(repository.findById(id).orElseThrow(() -> new ObjectNotFoundException(NOT_FOUND_MESSAGE)), StudentsDTO.class);
     }
 
     @Override
