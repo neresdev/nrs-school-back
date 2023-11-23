02532e21f3847c8575dc9aa362a5db3a9d35a132
Neres <neresdev@gmail.com>
2023-11-23 11:11:06 -0300

    feat: find all students

--- src/main/java/com/nrs/school/back/service/impl/StudentsServiceImpl.java
+++ src/main/java/com/nrs/school/back/service/impl/StudentsServiceImpl.java
@@ -2,13 +2,19 @@ package com.nrs.school.back.service.impl;
 
 import com.nrs.school.back.entities.Students;
 import com.nrs.school.back.entities.dto.StudentsDTO;
+import com.nrs.school.back.repository.StudentsRepository;
 import com.nrs.school.back.service.StudentsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class StudentsServiceImpl implements StudentsService {
+
+    @Autowired
+    private StudentsRepository repository;
+
     @Override
     public Students findById(Integer id) {
         return null;
@@ -16,7 +22,7 @@ public class StudentsServiceImpl implements StudentsService {
 
     @Override
     public List<Students> findAll() {
-        return List.of(new Students(1, "uai"), new Students(2, "uei"));
+        return repository.findAll();
     }
 
     @Override
