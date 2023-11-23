9c6849d34e233b765383cc7628ebd98f4f712146
Neres <neresdev@gmail.com>
2023-11-23 15:35:29 -0300

    moving mapper to service class and feature to find student by ID

--- src/main/java/com/nrs/school/back/controller/StudentController.java
+++ src/main/java/com/nrs/school/back/controller/StudentController.java
@@ -1,17 +1,15 @@
 package com.nrs.school.back.controller;
 
-import com.nrs.school.back.entities.Students;
 import com.nrs.school.back.entities.dto.StudentsDTO;
 import com.nrs.school.back.service.StudentsService;
-import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/api")
@@ -20,14 +18,15 @@ public class StudentController {
     private static final String ID = "/{id}";
 
     @Autowired
-    private ModelMapper mapper;
-
-    @Autowired
     private StudentsService service;
 
     @GetMapping("/all/students")
     public ResponseEntity<List<StudentsDTO>> findAll(){
-        return ResponseEntity.ok()
-                .body(service.findAll().stream().map(u -> mapper.map(u, StudentsDTO.class)).collect(Collectors.toList()));
+        return ResponseEntity.ok().body(service.findAll());
+    }
+
+    @GetMapping("/student" + ID)
+    public ResponseEntity<StudentsDTO> findStudentById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(service.findById(id));
     }
 }
