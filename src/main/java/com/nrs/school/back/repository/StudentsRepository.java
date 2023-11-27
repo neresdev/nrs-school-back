26908981f2a59a13386a157781da9262e1049eb8
Neres <neresdev@gmail.com>
2023-11-27 08:40:12 -0300

    adding methods to find and delete by registration

--- src/main/java/com/nrs/school/back/repository/StudentsRepository.java
+++ src/main/java/com/nrs/school/back/repository/StudentsRepository.java
@@ -1,11 +1,16 @@
 package com.nrs.school.back.repository;
 
 import com.nrs.school.back.entities.Students;
+
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface StudentsRepository extends JpaRepository<Students, Integer> {
+    Optional<Students> findByRegistration(String registration);
+    void deleteByRegistration(String registration);
 
 }
