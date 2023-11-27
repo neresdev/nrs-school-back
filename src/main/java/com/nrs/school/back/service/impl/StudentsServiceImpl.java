2dca58d326399f729264c62b1f25ed0b7d0020a7
Neres <neresdev@gmail.com>
2023-11-27 08:43:41 -0300

    static message variables, update, delete and find student by registration implementation

--- src/main/java/com/nrs/school/back/service/impl/StudentsServiceImpl.java
+++ src/main/java/com/nrs/school/back/service/impl/StudentsServiceImpl.java
@@ -2,6 +2,7 @@ package com.nrs.school.back.service.impl;
 
 import com.nrs.school.back.entities.Students;
 import com.nrs.school.back.entities.dto.StudentsDTO;
+import com.nrs.school.back.exceptions.DataIntegratyViolationException;
 import com.nrs.school.back.exceptions.ObjectNotFoundException;
 import com.nrs.school.back.repository.StudentsRepository;
 import com.nrs.school.back.service.StudentsService;
@@ -11,13 +12,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class StudentsServiceImpl implements StudentsService {
 
-    public static final String NOT_FOUND_MESSAGE = "Objeto nao encontrado";
+    public static final String NOT_FOUND_MESSAGE = "Student with registration %s not found";
+    public static final String JSON_INVALID_MESSAGE = "JSON invalid: ";
+    public static final String EXISTING_STUDENT_MESSAGE = "Student with registration %s already exists";
 
     @Autowired
     private StudentsRepository repository;
@@ -31,23 +34,27 @@ public class StudentsServiceImpl implements StudentsService {
     }
 
     @Override
-    public StudentsDTO findById(Integer id) {
-        return mapper.map(repository.findById(id).orElseThrow(() -> new ObjectNotFoundException(NOT_FOUND_MESSAGE)), StudentsDTO.class);
+    public StudentsDTO findByRegistration(String registration) {
+        return mapper.map(repository.findByRegistration(registration).orElseThrow(() -> new ObjectNotFoundException(String.format(NOT_FOUND_MESSAGE, registration))), StudentsDTO.class);
     }
 
     @Override
     public StudentsDTO create(StudentsDTO studentsDTO) {
-        // TODO fix error when insert new existent user
+        String messageValidator = entityValidator(studentsDTO);
+        Optional<Students> student = repository.findByRegistration(studentsDTO.getRegistration());
+        if(!messageValidator.isEmpty()) throw new DataIntegratyViolationException(JSON_INVALID_MESSAGE + messageValidator);
+        if(student.isPresent()) throw new DataIntegratyViolationException(EXISTING_STUDENT_MESSAGE + student.get().getRegistration());
         return mapper.map(repository.save(mapper.map(studentsDTO, Students.class)), StudentsDTO.class);
     }
 
     @Override
-    public Students update(StudentsDTO studentsDTO) {
-        return null;
+    public StudentsDTO update(StudentsDTO studentsDTO) {
+        studentsDTO.setId(findByRegistration(studentsDTO.getRegistration()).getId());
+        return mapper.map(repository.save(mapper.map(studentsDTO, Students.class)), StudentsDTO.class);
     }
 
     @Override
-    public void delete(Integer studentId) {
-
+    public void delete(String registration) {
+        repository.deleteById(findByRegistration(registration).getId());
     }
 }
