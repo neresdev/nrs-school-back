8d63f18fd12fc1b24cccc8ea46a4573f62c84895
neresdev <neresdev@gmail.com>
2025-03-17 21:16:24 -0300

    Users entity and repository

---
+++ src/main/java/com/nrs/school/back/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.nrs.school.back.repository;
+
+import com.nrs.school.back.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
