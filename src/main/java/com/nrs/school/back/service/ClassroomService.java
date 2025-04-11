7ee3238d6380493a6386a41e23976d652d5334de
neresdev <neresdev@gmail.com>
2025-04-11 14:24:48 -0300

    classsrom service

--- src/main/java/com/nrs/school/back/service/ClassroomService.java
+++ src/main/java/com/nrs/school/back/service/ClassroomService.java
@@ -1,4 +1,32 @@
 package com.nrs.school.back.service;
 
-public class ClassroomsService {
+import com.nrs.school.back.entities.dto.ClassroomDTO;
+import com.nrs.school.back.entities.dto.StudentDTO;
+import com.nrs.school.back.repository.ClassroomRepository;
+import com.nrs.school.back.repository.StudentRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class ClassroomService {
+
+    private final ClassroomRepository repository;
+
+    private final ModelMapper mapper;
+
+    public ClassroomService(ClassroomRepository repository, ModelMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public List<ClassroomDTO> findAll() {
+        return repository.findAll().stream().map(c -> {
+            final var classroomDTO = mapper.map(c, ClassroomDTO.class);
+            classroomDTO.setRequestId(UUID.randomUUID());
+            return classroomDTO;
+        }).toList();
+    }
 }
