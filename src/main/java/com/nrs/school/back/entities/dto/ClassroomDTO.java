4dd3cd166bf4bfd0c1c40890363d930803a3663f
neresdev <neresdev@gmail.com>
2025-04-11 14:21:39 -0300

    dto for classroom

--- src/main/java/com/nrs/school/back/entities/dto/ClassroomDTO.java
+++ src/main/java/com/nrs/school/back/entities/dto/ClassroomDTO.java
@@ -1,4 +1,123 @@
 package com.nrs.school.back.entities.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.mapstruct.BeforeMapping;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 public class ClassroomDTO {
+
+    @JsonIgnore
+    private Long id;
+
+    private String className;
+
+    private int capacity;
+
+    private String teacher;
+
+    private int shift;
+
+    private int classNumber;
+
+    private UUID requestId;
+
+    @JsonIgnore
+    private LocalDateTime createdAt;
+
+    @JsonIgnore
+    private LocalDateTime updatedAt;
+
+
+    public ClassroomDTO() {
+    }
+
+    public ClassroomDTO(Long id, String className, int capacity, String teacher, int shift, int classNumber, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.className = className;
+        this.capacity = capacity;
+        this.teacher = teacher;
+        this.shift = shift;
+        this.classNumber = classNumber;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public String getTeacher() {
+        return teacher;
+    }
+
+    public void setTeacher(String teacher) {
+        this.teacher = teacher;
+    }
+
+    public int getShift() {
+        return shift;
+    }
+
+    public void setShift(int shift) {
+        this.shift = shift;
+    }
+
+    public int getClassNumber() {
+        return classNumber;
+    }
+
+    public void setClassNumber(int classNumber) {
+        this.classNumber = classNumber;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(UUID requestId) {
+        this.requestId = requestId;
+    }
 }
