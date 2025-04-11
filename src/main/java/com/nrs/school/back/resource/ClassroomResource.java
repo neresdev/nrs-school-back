b1e6ae39f1415e3e43453af0da92c1f94317db18
neresdev <neresdev@gmail.com>
2025-04-11 14:24:19 -0300

    resource for classroom

--- src/main/java/com/nrs/school/back/resource/ClassroomResource.java
+++ src/main/java/com/nrs/school/back/resource/ClassroomResource.java
@@ -1,4 +1,27 @@
 package com.nrs.school.back.resource;
 
+
+import com.nrs.school.back.entities.dto.ClassroomDTO;
+import com.nrs.school.back.service.ClassroomService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/v1")
 public class ClassroomResource {
+
+    private final ClassroomService classroomService;
+
+    public ClassroomResource(ClassroomService classroomService) {
+        this.classroomService = classroomService;
+    }
+
+    @GetMapping("/classrooms")
+    public ResponseEntity<List<ClassroomDTO>> findAll() {
+        return ResponseEntity.ok(classroomService.findAll());
+    }
 }
