5596a86254307e66ee99f731785eb6363da0fb63
neresdev <neresdev@gmail.com>
2025-04-13 21:43:24 -0300

    method to check if classroom exists

--- src/main/java/com/nrs/school/back/repository/ClassroomRepository.java
+++ src/main/java/com/nrs/school/back/repository/ClassroomRepository.java
@@ -6,4 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClassroomRepository extends JpaRepository<Classroom, Long> {
+    boolean existsByClassroomName(String className);
 }
--- src/main/java/com/nrs/school/back/service/ClassroomService.java
+++ src/main/java/com/nrs/school/back/service/ClassroomService.java
@@ -27,4 +27,8 @@ public class ClassroomService {
             return classroomDTO;
         }).toList();
     }
+
+    public boolean existsClassroom(String className) {
+        return repository.existsByClassroomName(className);
+    }
 }
