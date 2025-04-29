e21993a21f81ca88c5c6cb43c106a77d553c0806
neresdev <neresdev@gmail.com>
2025-04-29 09:43:44 -0300

    adding description

--- src/main/java/com/nrs/school/back/enm/StudentError.java
+++ src/main/java/com/nrs/school/back/enm/StudentError.java
@@ -2,15 +2,21 @@ package com.nrs.school.back.enm;
 
 
 public enum StudentError {
-    STUDENT_CLASSROOM_NOT_FOUND("S001");
+    STUDENT_CLASSROOM_NOT_FOUND("S001", "Student classroom does not exists");
 
     private String code;
+    private String description;
 
-    StudentError(String code) {
+    StudentError(String code, String description) {
         this.code = code;
+        this.description = description;
     }
 
     public String getCode() {
         return code;
+    }
+
+    public String getDescription() {
+        return description;
     }
 }
