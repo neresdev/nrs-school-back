86e9f8b4b6affa7bd55b1dd8393035775f41212c
neresdev <neresdev@gmail.com>
2025-05-02 09:57:11 -0300

    endpoint not mapped costom resolver

--- src/main/java/com/nrs/school/back/config/CustomExceptionResolver.java
+++ src/main/java/com/nrs/school/back/config/CustomExceptionResolver.java
@@ -8,12 +8,18 @@ import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.security.SignatureException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.lang.Nullable;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
@@ -22,6 +28,7 @@ import java.util.Map;
 import java.util.logging.Logger;
 
 @Component
+@ControllerAdvice
 public class CustomExceptionResolver implements HandlerExceptionResolver {
     public static final Logger LOGGER = Logger.getLogger(CustomExceptionResolver.class.getName());
 
@@ -40,11 +47,8 @@ public class CustomExceptionResolver implements HandlerExceptionResolver {
         response.setContentType("application/json");
 
         Map<String, Object> responseObject = new HashMap<>();
-        responseObject.put("timestamp", LocalDateTime.now().toString());
-        responseObject.put("message", exception.getMessage());
-        responseObject.put("path", request.getRequestURI());
 
-        handleException(exception, response, responseObject);
+        handleException(exception, response, responseObject, request);
 
         try {
             String json = objectMapper.writeValueAsString(responseObject);
@@ -54,9 +58,14 @@ public class CustomExceptionResolver implements HandlerExceptionResolver {
         return new ModelAndView();
     }
 
-    private void handleException(Exception exception, HttpServletResponse response, Map<String, Object> responseObject) {
+    private void handleException(Exception exception,
+                                 HttpServletResponse response,
+                                 Map<String, Object> responseObject,
+                                 HttpServletRequest request) {
         LOGGER.info("Request error: " + exception.getMessage());
 
+        responseObject.put("timestamp", LocalDateTime.now().toString());
+        responseObject.put("path", request.getRequestURI());
         if (exception instanceof ObjectNotFoundException) {
             response.setStatus(HttpServletResponse.SC_NOT_FOUND);
             responseObject.put("error", "Path not found");
@@ -78,20 +87,33 @@ public class CustomExceptionResolver implements HandlerExceptionResolver {
             responseObject.put("error", "The JWT token has expireda");
             responseObject.put("status", HttpServletResponse.SC_FORBIDDEN);
         } else if (exception instanceof MissingAuthorizationException) {
-            response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             responseObject.put("error", exception.getMessage());
-            responseObject.put("status", HttpServletResponse.SC_FORBIDDEN);
+            responseObject.put("status", HttpServletResponse.SC_UNAUTHORIZED);
         } else if (exception instanceof StudentClassroomNotFoundException) {
             response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
             responseObject.put("error",  exception.getMessage());
             responseObject.put("status", HttpServletResponse.SC_BAD_REQUEST);
             responseObject.put("errorCode", ((StudentClassroomNotFoundException) exception).getCode());
+        } else if (exception instanceof NoHandlerFoundException) {
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+            responseObject.put("error",  exception.getMessage());
+            responseObject.put("status", HttpServletResponse.SC_NOT_FOUND);
         } else {
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             responseObject.put("error", DEFAULT_ERROR_MESSAGE);
             responseObject.put("status", HttpServletResponse.SC_UNAUTHORIZED);
-            responseObject.put("message", DEFAULT_ERROR_MESSAGE);
         }
+    }
+
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseBody
+    public Map<String, Object> handleNotFound(NoHandlerFoundException exception, HttpServletRequest request, HttpServletResponse response) {
+        Map<String, Object> responseObject = new HashMap<>();
+        handleException(exception, response, responseObject, request);
+        return responseObject;
     }
 
 }
