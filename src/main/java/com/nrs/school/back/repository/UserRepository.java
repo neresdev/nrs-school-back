5af3e2d0e228cfe35a0f0797dbd3f271f1dfba58
neresdev <neresdev@gmail.com>
2025-08-03 11:44:00 -0300

    User to UserEntity

--- src/main/java/com/nrs/school/back/repository/UserRepository.java
+++ src/main/java/com/nrs/school/back/repository/UserRepository.java
@@ -1,10 +1,10 @@
 package com.nrs.school.back.repository;
 
-import com.nrs.school.back.entities.User;
+import com.nrs.school.back.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
 }
