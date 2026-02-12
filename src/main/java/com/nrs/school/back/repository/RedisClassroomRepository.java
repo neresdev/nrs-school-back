5bcfdc28e11a8af51b45d64bdc1ef3515ba4c9bc
neresdev <neresdev@gmail.com>
2026-02-12 16:19:11 -0300

    deleting redis configuration and usage

--- src/main/java/com/nrs/school/back/repository/RedisClassroomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nrs.school.back.repository;
-
-import com.nrs.school.back.entities.RedisClassroomEntity;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface RedisClassroomRepository extends CrudRepository<RedisClassroomEntity, String> {
-}
