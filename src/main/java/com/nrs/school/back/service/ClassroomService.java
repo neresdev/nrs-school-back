c762b2dd535b68c8570386c5d1716bbfddc2768b
neresdev <neresdev@gmail.com>
2026-02-19 21:24:29 -0300

    using UUID instead of String

--- src/main/java/com/nrs/school/back/service/ClassroomService.java
+++ src/main/java/com/nrs/school/back/service/ClassroomService.java
@@ -1,12 +1,10 @@
 package com.nrs.school.back.service;
 
 import com.nrs.school.back.entities.ClassroomEntity;
-import com.nrs.school.back.entities.RedisClassroomEntity;
 import com.nrs.school.back.entities.dto.ClassroomDTO;
 import com.nrs.school.back.exceptions.DataIntegrityViolationException;
 import com.nrs.school.back.exceptions.ObjectNotFoundException;
 import com.nrs.school.back.repository.ClassroomRepository;
-import com.nrs.school.back.repository.RedisClassroomRepository;
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.Validation;
 import jakarta.validation.Validator;
@@ -15,7 +13,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
-import java.util.stream.StreamSupport;
 
 @Component
 public class ClassroomService {
@@ -25,43 +22,15 @@ public class ClassroomService {
     private static final String CLASSROOM_NOT_FOUND_MESSAGE = "Classroom with id %s not found";
 
     private final ClassroomRepository repository;
-    private final RedisClassroomRepository redisRepository;
     private final ModelMapper mapper;
 
-    public ClassroomService(ClassroomRepository repository, RedisClassroomRepository redisRepository, ModelMapper mapper) {
+    public ClassroomService(ClassroomRepository repository, ModelMapper mapper) {
         this.repository = repository;
         this.mapper = mapper;
-        this.redisRepository = redisRepository;
     }
 
     public List<ClassroomDTO> findAll() {
-        final var classroomsDto = this.redisClassrooms();
-        if (!classroomsDto.isEmpty()) {
-            return classroomsDto;
-        }
-
-        final var classRoomEntities = repository.findAll();
-
-        saveRedisClassrooms(classRoomEntities);
-
-        return classRoomEntities.stream().map(c -> mapper.map(c, ClassroomDTO.class)).toList();
-    }
-
-    private List<ClassroomDTO> redisClassrooms() {
-        final var classroomsDto = new ArrayList<ClassroomDTO>();
-        final var classroomEntities = redisRepository.findAll();
-
-        for (RedisClassroomEntity classroomEntity : classroomEntities) {
-            if(Objects.nonNull(classroomEntity)) {
-                classroomsDto.add(mapper.map(classroomEntity, ClassroomDTO.class));
-            }
-        }
-
-        return classroomsDto;
-    }
-
-    private void saveRedisClassrooms(List<ClassroomEntity> classroomEntities) {
-        redisRepository.saveAll(classroomEntities.stream().map(c -> mapper.map(c, RedisClassroomEntity.class)).toList());
+        return repository.findAll().stream().map(c -> mapper.map(c, ClassroomDTO.class)).toList();
     }
 
     public ClassroomDTO create(ClassroomDTO classroomDTO) {
@@ -94,7 +63,7 @@ public class ClassroomService {
     }
 
     public ClassroomDTO findByClassroomId(String classroomId) {
-        var classroom = repository.findByClassroomReferenceCode(classroomId);
+        var classroom = repository.findByClassroomReferenceCode(UUID.fromString(classroomId));
         if (classroom.isEmpty()) {
             throw new ObjectNotFoundException(CLASSROOM_NOT_FOUND_MESSAGE.formatted(classroomId));
         }
