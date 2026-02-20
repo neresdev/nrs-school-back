7a376e5f924cdbc88899fc43df5406a21e220a70
neresdev <neresdev@gmail.com>
2026-02-19 22:09:33 -0300

    updating cucumber configuration

--- src/test/java/com/nrs/school/back/StepDefinitionsDefault.java
+++ src/test/java/com/nrs/school/back/StepDefinitionsDefault.java
@@ -2,10 +2,12 @@ package com.nrs.school.back;
 
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.ActiveProfiles;
 
 @CucumberContextConfiguration
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        classes = Application.class)
 @ActiveProfiles("test")
-public class SpringIntegrationTest {
+public class StepDefinitionsDefault {
 }
