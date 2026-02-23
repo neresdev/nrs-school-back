fac4110772dd9d3cc66892b8abb11739b16ba718
neresdev <neresdev@gmail.com>
2026-02-22 22:05:47 -0300

    steps for authentication scenarios

--- src/test/java/com/nrs/school/back/steps/AuthSteps.java
+++ src/test/java/com/nrs/school/back/steps/AuthSteps.java
@@ -2,20 +2,26 @@ package com.nrs.school.back.steps;
 
 import com.nrs.school.back.StepDefinitionsDefault;
 import com.nrs.school.back.fixtures.TestApiFixtures;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 
 import java.net.URISyntaxException;
 
-public class ApiSteps extends StepDefinitionsDefault {
+public class AuthSteps extends StepDefinitionsDefault {
 
     private final TestApiFixtures testApiFixtures;
 
-    public ApiSteps(TestApiFixtures testApiFixtures) {
+    public AuthSteps(TestApiFixtures testApiFixtures) {
         this.testApiFixtures = testApiFixtures;
     }
 
     @Given("authentication was performed")
     public void authentication() throws URISyntaxException {
         testApiFixtures.authentication();
+    }
+
+    @And("login was performed")
+    public void login() throws URISyntaxException {
+        testApiFixtures.login();
     }
 }
