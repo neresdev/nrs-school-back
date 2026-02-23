a4e8014a2f5d0f3a18099db95a7c8993bff58630
neresdev <neresdev@gmail.com>
2026-02-22 22:05:47 -0300

    wip: fix scenarios to make http requests for api

--- src/test/java/com/nrs/school/back/steps/StudentResourceSteps.java
+++ src/test/java/com/nrs/school/back/steps/StudentResourceSteps.java
@@ -1,17 +1,19 @@
 package com.nrs.school.back.steps;
 
 import com.nrs.school.back.StepDefinitionsDefault;
-import com.nrs.school.back.entities.dto.StudentDTO;
+import com.nrs.school.back.entities.dto.students.StudentDataRequest;
+import com.nrs.school.back.entities.dto.students.StudentDataResponse;
+import com.nrs.school.back.entities.dto.students.StudentResponse;
 import com.nrs.school.back.exceptions.DataIntegrityViolationException;
 import com.nrs.school.back.exceptions.ObjectNotFoundException;
+import com.nrs.school.back.fixtures.TestApiFixtures;
 import com.nrs.school.back.resource.StudentResource;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 import java.util.Map;
@@ -25,47 +27,40 @@ public class StudentResourceSteps extends StepDefinitionsDefault {
 
     private final StudentResource studentResource;
 
-    private ResponseEntity<List<StudentDTO>> studentsReturned;
+    private ResponseEntity<StudentResponse> studentsResponse;
 
-    private ResponseEntity<StudentDTO> studentReturned;
+    private ResponseEntity<StudentDataResponse> studentDataResponse;
 
-    private ObjectNotFoundException objectNotFound;
-
-    private ResponseEntity<StudentDTO> studentCreated;
+    private ResponseEntity<StudentDataResponse> studentCreated;
 
     private DataIntegrityViolationException dataIntegrityViolationInvalidEmailException;
 
     private DataIntegrityViolationException dataIntegrityViolationExistingRegistrationException;
 
-    private ResponseEntity<StudentDTO> studentUpdated;
+    private ResponseEntity<StudentDataResponse> studentUpdated;
 
-    private final TestRestTemplate testRestTemplate;
+    private final TestApiFixtures testApiFixtures;
 
-    public StudentResourceSteps(StudentResource studentResource, TestRestTemplate testRestTemplate) {
+    public StudentResourceSteps(StudentResource studentResource, TestApiFixtures testApiFixtures) {
         this.studentResource = studentResource;
-        this.testRestTemplate = testRestTemplate;
+        this.testApiFixtures = testApiFixtures;
     }
 
-    @When("find student by registration {string}")
-    public void findStudentByRegistration(String registration) {
-        try {
-            this.studentReturned = studentResource.findStudentByRegistration(registration);
-        } catch (ObjectNotFoundException e){
-            this.objectNotFound = e;
-        }
-
-    }
-
-    @When("find all students")
+    @When("the client sends a GET http request get all students")
     public void findAllStudent() throws URISyntaxException {
-        final var response = testRestTemplate.getForEntity(new URI(StudentResource.BASE_PATH), Object.class); // todo wip
-        this.studentsReturned = studentResource.findAll();
+        this.studentsResponse = testApiFixtures.makeGetRequest(StudentResource.BASE_PATH, StudentResponse.class);
+    }
+
+    @When("the client sends a GET http request a student with registration {string}")
+    public void findStudentByRegistration(String registration) throws URISyntaxException {
+        final var registrationPath = "/" + registration;
+        studentDataResponse = testApiFixtures.makeGetRequest(StudentResource.BASE_PATH + registrationPath, StudentDataResponse.class);
     }
 
     @When("create a student")
-    public void createStudent(List<Map<String, String>> student) {
+    public void createStudent(List<Map<String, String>> student) { // todo wip
         try {
-            this.studentCreated = this.studentResource.create(convertFeatureDataToStudentDto(student.get(0)));
+            this.studentCreated = this.studentResource.create(convertFeatureDataToStudentRequest(student.get(0)));
         } catch (DataIntegrityViolationException e) {
             this.dataIntegrityViolationInvalidEmailException = e;
         }
@@ -73,20 +68,19 @@ public class StudentResourceSteps extends StepDefinitionsDefault {
 
     @When("update student")
     public void updateStudent(List<Map<String, String>> expectedData) {
-        var studentToUpdate = convertFeatureDataToStudentDto(expectedData.get(0));
+        var studentToUpdate = convertFeatureDataToStudentRequest(expectedData.get(0));
         this.studentUpdated = studentResource.update(studentToUpdate);
     }
 
     @When("delete student by registration {string}")
     public void deleteStudentByRegistration(String registration) {
-        int a = 0;
         this.studentResource.delete(registration);
     }
 
     @And("create a student with existing registration")
     public void createAStudentWithExistingRegistration(List<Map<String, String>> student) {
         try {
-            this.studentResource.create(convertFeatureDataToStudentDto(student.get(0)));
+            this.studentResource.create(convertFeatureDataToStudentRequest(student.get(0)));
         } catch (DataIntegrityViolationException e){
             this.dataIntegrityViolationExistingRegistrationException = e;
         }
@@ -95,24 +89,25 @@ public class StudentResourceSteps extends StepDefinitionsDefault {
 
     @Then("return all students")
     public void returnAllStudents(List<Map<String, String>> expectedData) {
-        final var expectedStudents = expectedData.stream().map(this::convertFeatureDataToStudentDto).toList();
+        final var expectedStudents = expectedData.stream().map(this::convertFeatureDataToStudentResponse).toList();
 
-        assertTrue(studentsReturned.getStatusCode().is2xxSuccessful());
+        assertTrue(studentsResponse.getStatusCode().is2xxSuccessful());
 
-        Objects.requireNonNull(studentsReturned.getBody())
-                .forEach(student ->
-                        assertFields(expectedStudents
-                                        .stream()
-                                        .filter(s -> Objects.equals(s.getStudentId(), student.getStudentId())).findFirst()
-                                        .orElseThrow(()-> new ObjectNotFoundException(NOT_FOUND_MESSAGE.formatted(student.getRegistration()))),
-                                student));
+        Objects.requireNonNull(studentsResponse.getBody())
+            .getStudents()
+            .forEach(student -> assertFields(expectedStudents.stream().filter(
+                    s -> Objects.equals(s.getRegistration(), student.getRegistration()))
+                            .findFirst().orElseThrow(()-> new ObjectNotFoundException(NOT_FOUND_MESSAGE.formatted(student.getRegistration()))),
+                        student));
 
     }
 
     @Then("return a student")
     public void returnAStudent(List<Map<String, String>> expectedData) {
-        final var expectedStudent = expectedData.stream().map(this::convertFeatureDataToStudentDto).toList().get(0);
-        final var actualStudent = studentReturned.getBody();
+        final var expectedStudent = expectedData.stream().map(this::convertFeatureDataToStudentResponse).toList().get(0);
+
+        assertTrue(studentDataResponse.getStatusCode().is2xxSuccessful());
+        final var actualStudent = studentDataResponse.getBody();
 
         assertNotNull(actualStudent);
         assertFields(expectedStudent, actualStudent);
@@ -120,7 +115,7 @@ public class StudentResourceSteps extends StepDefinitionsDefault {
 
     @Then("throw an student not found with registration {string} not found")
     public void throwAnException(String registration) {
-        assertEquals(objectNotFound.getMessage(), NOT_FOUND_MESSAGE.formatted(registration));
+        assertEquals(HttpStatus.NOT_FOUND, studentDataResponse.getStatusCode());
     }
 
     @Then("return a url form the student created")
@@ -143,8 +138,8 @@ public class StudentResourceSteps extends StepDefinitionsDefault {
 
     @Then("return a url form the student with fields updated")
     public void thenReturnAUrlFromStudentUpdated(List<Map<String, String>> expectedData) {
-        var expectedStudent = this.convertFeatureDataToStudentDto(expectedData.get(0));
-        final var expectedLocation = "http://localhost:8080/api/v1/get/student/%s".formatted(expectedStudent.getStudentId());
+        var expectedStudent = this.convertFeatureDataToStudentResponse(expectedData.get(0));
+        final var expectedLocation = "http://localhost:8080/api/v1/get/student/%s".formatted(expectedStudent.getRegistration());
         var student = this.studentResource.findStudentByRegistration(expectedStudent.getRegistration());
 
         assertEquals(expectedLocation, Objects.requireNonNull(this.studentUpdated.getHeaders().get("Location")).get(0));
@@ -163,22 +158,31 @@ public class StudentResourceSteps extends StepDefinitionsDefault {
 
     }
 
-    private static void assertFields(StudentDTO expected, StudentDTO actual) {
-        assertEquals(expected.getStudentId(), actual.getStudentId());
+    private static void assertFields(StudentDataResponse expected, StudentDataResponse actual) {
         assertEquals(expected.getStudentName(), actual.getStudentName());
         assertEquals(expected.getStudentEmail(), actual.getStudentEmail());
         assertEquals(expected.getClassroomName(), actual.getClassroomName());
         assertEquals(expected.getRegistration(), actual.getRegistration());
     }
 
-    private StudentDTO convertFeatureDataToStudentDto(Map<String, String> data){
-        return new StudentDTO(
-                Long.valueOf(data.get("studentId")),
+    private StudentDataResponse convertFeatureDataToStudentResponse(Map<String, String> data){
+        return new StudentDataResponse(
                 data.get("studentName"),
                 data.get("studentEmail"),
                 data.get("classroomName"),
                 data.get("registration")
         );
     }
+
+    private StudentDataRequest convertFeatureDataToStudentRequest(Map<String, String> data){
+        return new StudentDataRequest(
+                data.get("studentName"),
+                data.get("studentEmail"),
+                data.get("classroomName"),
+                data.get("registration")
+        );
+    }
+
+
 
 }
