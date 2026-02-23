096312659c17ab9a03ee929aa290d9599744db0b
neresdev <neresdev@gmail.com>
2026-02-22 22:05:47 -0300

    fix imports

--- src/test/java/com/nrs/school/back/StepDefinitionsDefault.java
+++ src/test/java/com/nrs/school/back/StepDefinitionsDefault.java
@@ -1,6 +1,5 @@
 package com.nrs.school.back;
 
-import com.nrs.school.back.fixtures.TestApiFixtures;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -9,10 +8,5 @@ import org.springframework.test.context.ActiveProfiles;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
         classes = Application.class)
 @ActiveProfiles("test")
-public class StepDefinitionsApiDefault<T> {
-    private final TestApiFixtures testApiFixtures;
-
-    public StepDefinitionsApiDefault(TestApiFixtures testApiFixtures) {
-        this.testApiFixtures = testApiFixtures;
-    }
+public class StepDefinitionsDefault {
 }
